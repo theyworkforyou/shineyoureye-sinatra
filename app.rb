f95bf30545a607f00bade9d334bf869d238a2003
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-12-07 19:19:14 +0000

    Use the everypolitician gem to display all areas for Nigeria
    This step was made to check if we can pull EveryPolitician data
    and render it in the views. We can.

--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
+require 'everypolitician'
+
+set :datasource, ENV.fetch('DATASOURCE', 'https://github.com/everypolitician/everypolitician-data/raw/master/countries.json')
+set :index, EveryPolitician::Index.new(index_url: settings.datasource)
+set :country, settings.index.country('Nigeria')
 
 get '/' do
   erb :homepage
+end
+
+get '/places/' do
+  @country = settings.country
+  erb :places
 end
