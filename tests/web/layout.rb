1542d5c0e35e5098bc50ae0ece76faebedc3466b
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-12-09 16:16:52 +0000

    Add tests for the layout view using minitest

---
+++ tests/web/layout.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+require 'test_helper'
+require_relative '../../app'
+
+describe 'Layout' do
+  before  { get '/' }
+  subject { Nokogiri::HTML(last_response.body) }
+
+  it 'includes the head' do
+    refute_empty(subject.css('title').text)
+  end
+
+  it 'includes the main menu' do
+    refute_empty(subject.css('#main-menu'))
+  end
+
+  it 'includes the header' do
+    refute_empty(subject.css('header menu'))
+  end
+
+  it 'includes the footer' do
+    refute_empty(subject.css('#site-footer'))
+  end
+end
