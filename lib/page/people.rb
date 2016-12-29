85a8cf586dd1718c918d7864cb0e47284e3435a9
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-12-29 21:49:22 +0000

    Add a presenter page for a list of people view
    This page works as a presenter, using PeopleByLegislature to get
    a list of representatives.

---
+++ lib/page/people.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+module Page
+  class People
+    attr_reader :title
+
+    def initialize(title:, people_by_legislature:)
+      @title = title
+      @people_by_legislature = people_by_legislature
+    end
+
+    def all
+      @all ||= people_by_legislature.find_all
+    end
+
+    private
+
+    attr_reader :people_by_legislature
+  end
+end
