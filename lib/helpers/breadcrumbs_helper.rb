dcf5283c999f616a56b92ea8b951503717194ce6
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-01-07 01:09:50 +0000

    Use the breadcrumbs helper to inject the breadcrumbs in the views
    The helper now uses the breadcrumb builder and takes the current
    page url from Sinatra's request object.
    
    The helper also has a method to disable breadcrumbs for specific
    pages, for example, the home page.

--- lib/helpers/breadcrumbs_helper.rb
+++ lib/helpers/breadcrumbs_helper.rb
@@ -1,10 +1,31 @@
 # frozen_string_literal: true
+require_relative '../breadcrumbs/builder'
+require_relative '../breadcrumbs/route_to_display_text_wrapper'
+
 module BreadcrumbsHelper
+  def breadcrumbs
+    builder.breadcrumbs
+  end
+
+  def disable_breadcrumbs_for_current_page?
+    is_homepage?
+  end
+
+  private
+
   ROUTE_TO_DISPLAY_TEXT = {
     '/' => 'Home',
     '/blog/' => 'Blog',
     '/info/events' => 'Events',
   }
+
+  def builder
+    Breadcrumbs::Builder.new(current_url: request.url, routes_wrapper: wrapper)
+  end
+
+  def wrapper
+    Breadcrumbs::RouteToDisplayTextWrapper.new(routes: ROUTE_TO_DISPLAY_TEXT)
+  end
 end
 
 helpers BreadcrumbsHelper
