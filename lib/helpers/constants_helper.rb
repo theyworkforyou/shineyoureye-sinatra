c33ad0315c0aa6bb4240eca569f9aaf8b7094f39
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-01-18 09:59:26 +0000

    Add a path and a filename pattern to the constants helper

--- lib/helpers/constants_helper.rb
+++ lib/helpers/constants_helper.rb
@@ -3,6 +3,10 @@ module ConstantsHelper
     settings.content_dir
   end
 
+  def events_dir
+    "#{content_dir}/events"
+  end
+
   def info_dir
     "#{content_dir}/info"
   end
@@ -11,16 +15,24 @@ module ConstantsHelper
     "#{content_dir}/posts"
   end
 
+  def events_pattern
+    "#{events_dir}/#{date_glob}-*.md"
+  end
+
+  def event_pattern(slug)
+    "#{events_dir}/#{date_glob}-#{slug}.md"
+  end
+
+  def info_pattern(slug)
+    "#{info_dir}/#{slug}.md"
+  end
+
   def posts_pattern
     "#{posts_dir}/#{date_glob}-*.md"
   end
 
   def post_pattern(slug)
     "#{posts_dir}/#{date_glob}-#{slug}.md"
-  end
-
-  def info_pattern(slug)
-    "#{info_dir}/#{slug}.md"
   end
 
   private
