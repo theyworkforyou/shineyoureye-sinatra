905ea684934b1423674200879b9dac57c510e8f6
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-02-10 12:38:49 +0000

    Update the test helper so that it uses the new Senate fixtures

--- tests/test_helper.rb
+++ tests/test_helper.rb
@@ -23,6 +23,7 @@ module Minitest
     before do
       get_from_disk(DATASOURCE, countries_json)
       get_from_disk(reps_json_url, reps_json)
+      get_from_disk(senate_json_url, senate_json)
       get_from_disk("#{mapit_url}STA", STA_JSON)
       get_from_disk("#{mapit_url}FED", FED_JSON)
       get_from_disk("#{mapit_url}SEN", SEN_JSON)
@@ -52,8 +53,9 @@ module Minitest
     DATASOURCE = 'https://github.com/everypolitician/everypolitician-data/raw/master/countries.json'
     REPO_URL = 'https://cdn.rawgit.com/everypolitician/everypolitician-data'
     DISK_PATH = 'tests/fixtures/ep_data'
-    COUNTRIES_COMMIT = '2dd531c'
+    COUNTRIES_COMMIT = '62e6190'
     REPS_COMMIT = 'd5a1eb7'
+    SENATE_COMMIT = '9b61497'
 
     def get_from_disk(url, json_file)
       stub_request(:get, url).to_return(body: json_file)
@@ -63,12 +65,20 @@ module Minitest
       "#{REPO_URL}/#{REPS_COMMIT}/data/Nigeria/Representatives/ep-popolo-v1.0.json"
     end
 
+    def senate_json_url
+      "#{REPO_URL}/#{SENATE_COMMIT}/data/Nigeria/Senate/ep-popolo-v1.0.json"
+    end
+
     def countries_json
       @countries_json ||= File.read("#{DISK_PATH}/#{COUNTRIES_COMMIT}/countries.json")
     end
 
     def reps_json
       @reps_json ||= File.read("#{DISK_PATH}/#{REPS_COMMIT}/ep-popolo-v1.0.json")
+    end
+
+    def senate_json
+      @senate_json ||= File.read("#{DISK_PATH}/#{SENATE_COMMIT}/ep-popolo-v1.0.json")
     end
   end
 end
