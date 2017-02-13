c4cc01a367a42b740901e4e537142474ee448faf
Zarino Zappia <mail@zarino.co.uk>
2017-02-13 11:04:59 +0000

    Styling for person profile page and shared social widget
    [This includes changes by Mark Longair to adjust the tests accordingly:
    
     * At the moment, to get a full sized version of the image, we're
       hotlinking to the images on the National Assembly website. We should
       fix that very soon: see
       https://github.com/theyworkforyou/shineyoureye-sinatra/issues/33 for
       the proposed solution. However, to match the changes in the views
       I've changed the tests to expect the National Assembly website image
       URLs.
    
     * I removed the test checking for a srcset present, since Zarino
       removed that attribute.
    
     * I removed the test checking for a link to the comments, since such a
       link is no longer in the page design.]

--- tests/web/person.rb
+++ tests/web/person.rb
@@ -11,22 +11,17 @@ describe 'Person Page' do
   end
 
   it 'shows the person name' do
-    subject.css('.title-space__name h1').text.must_equal('ABDUKADIR RAHIS')
+    subject.css('h1.person__name').text.must_equal('ABDUKADIR RAHIS')
   end
 
   describe 'when person has an image' do
     it 'points to the right path' do
-      subject.css('.profile-pic img/@src').first.text
-        .must_include('/b2a7f72a-9ecf-4263-83f1-cb0f8783053c/140x140.jpeg')
-    end
-
-    it 'displays an srcset' do
-      subject.css('.profile-pic img/@srcset').first.text
-        .must_include('/b2a7f72a-9ecf-4263-83f1-cb0f8783053c/140x140.jpeg')
+      subject.css('img.person__image/@src').first.text
+        .must_equal('http://www.nass.gov.ng/images/mps/546.jpg')
     end
 
     it 'displays the name as alternative text' do
-      subject.css('.profile-pic img/@alt').first.text.must_equal('ABDUKADIR RAHIS')
+      subject.css('img.person__image/@alt').first.text.must_equal('ABDUKADIR RAHIS')
     end
   end
 
@@ -35,27 +30,27 @@ describe 'Person Page' do
     subject { Nokogiri::HTML(last_response.body) }
 
     it 'shows a picture anyway (empty avatar)' do
-      subject.css('.profile-pic img/@src').first.text
-        .must_include('/764fce72-c12a-42ad-ba84-d899f81f7a77/140x140.jpeg')
+      subject.css('img.person__image/@src').first.text
+        .must_equal('http://www.nass.gov.ng/images/mps/avatar.jpg')
     end
   end
 
   describe 'when person has an area' do
     it 'links to the area page' do
-      subject.css('.constituency-party a/@href').first.text.must_equal('/place/maiduguri/')
+      subject.css('.person__area a/@href').first.text.must_equal('/place/maiduguri/')
     end
 
     it 'displays the area name' do
-      subject.css('.constituency-party a').first.text.must_equal('Maiduguri (Metropolitan)')
+      subject.css('.person__area a').first.text.must_equal('Maiduguri (Metropolitan)')
     end
   end
 
   it 'links to the party page' do
-    subject.css('.party-memberships a/@href').first.text.must_equal('/organisation/apc/')
+    subject.css('.person__party a/@href').first.text.must_equal('/organisation/apc/')
   end
 
   it 'displays the party name' do
-    subject.css('.party-memberships a').first.text.must_equal('All Progressives Congress')
+    subject.css('.person__party a').first.text.must_equal('All Progressives Congress')
   end
 
   describe 'if person id does not exist in this legislature' do
@@ -69,17 +64,14 @@ describe 'Person Page' do
 
   describe 'social block' do
     it 'links to facebook share' do
-      subject.css('.big-btn-text-fb/@href').text.must_include('/person/b2a7f72a-9ecf-4263-83f1-cb0f8783053c/&t=ABDUKADIR RAHIS')
+      subject.css('.btn-facebook/@href').text.must_include('/person/b2a7f72a-9ecf-4263-83f1-cb0f8783053c/&t=ABDUKADIR RAHIS')
     end
 
     it 'links to twitter share' do
-      subject.css('.big-btn-text-tw/@href').text.must_include('NGShineyoureye')
-      subject.css('.big-btn-text-tw/@href').text.must_include('&text=ABDUKADIR RAHIS')
-      subject.css('.big-btn-text-tw/@href').text.must_include('/person/b2a7f72a-9ecf-4263-83f1-cb0f8783053c/')
+      subject.css('.btn-twitter/@href').text.must_include('NGShineyoureye')
+      subject.css('.btn-twitter/@href').text.must_include('&text=ABDUKADIR RAHIS')
+      subject.css('.btn-twitter/@href').text.must_include('/person/b2a7f72a-9ecf-4263-83f1-cb0f8783053c/')
     end
 
-    it 'links to comments' do
-      subject.css('.big-btn-text-green/@href').text.must_include('/person/b2a7f72a-9ecf-4263-83f1-cb0f8783053c/#comments')
-    end
   end
 end
