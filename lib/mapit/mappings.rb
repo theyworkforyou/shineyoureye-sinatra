caef5621f23e76516383397b336b31ea84809e97
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-02-14 14:41:03 +0000

    Use the Mapit to EP area mappings for senatorial districts
    This commit updates the mappings class and the instance creation
    in the filepaths helper so that it uses the CSV file with the mappings
    of Mapit areas to EP areas for senatorial districts.

--- lib/mapit/mappings.rb
+++ lib/mapit/mappings.rb
@@ -6,11 +6,13 @@ module Mapit
     def initialize(
       fed_to_sta_ids_mapping_filename:,
       pombola_slugs_to_mapit_ids_filename:,
-      mapit_to_ep_areas_fed_filename:
+      mapit_to_ep_areas_fed_filename:,
+      mapit_to_ep_areas_sen_filename:
     )
       @fed_to_sta_ids_mapping_filename = fed_to_sta_ids_mapping_filename
       @pombola_slugs_to_mapit_ids_filename = pombola_slugs_to_mapit_ids_filename
       @mapit_to_ep_areas_fed_filename = mapit_to_ep_areas_fed_filename
+      @mapit_to_ep_areas_sen_filename = mapit_to_ep_areas_sen_filename
     end
 
     def fed_to_sta_mapping
@@ -22,19 +24,24 @@ module Mapit
     end
 
     def ep_to_mapit_ids
-      @ep_to_mapit_ids ||= reverse_ep_to_mapit_ids_fed.to_h
+      @ep_to_mapit_ids ||= reverse_ep_to_mapit_ids.to_h
     end
 
     private
 
-    attr_reader :fed_to_sta_ids_mapping_filename, :pombola_slugs_to_mapit_ids_filename, :mapit_to_ep_areas_fed_filename
+    attr_reader :fed_to_sta_ids_mapping_filename, :pombola_slugs_to_mapit_ids_filename,
+                :mapit_to_ep_areas_fed_filename, :mapit_to_ep_areas_sen_filename
 
     def reverse_pombola_slugs_to_mapit_ids
       CSV.read(pombola_slugs_to_mapit_ids_filename).map { |row| [row[1], row.first] }
     end
 
-    def reverse_ep_to_mapit_ids_fed
-      CSV.read(mapit_to_ep_areas_fed_filename).map { |row| row.reverse }
+    def reverse_ep_to_mapit_ids
+      reverse(mapit_to_ep_areas_fed_filename) + reverse(mapit_to_ep_areas_sen_filename)
+    end
+
+    def reverse(mapping_filename)
+      CSV.read(mapping_filename).map { |row| row.reverse }
     end
   end
 end
