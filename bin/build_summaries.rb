2348fe9092f53b161439caf2f062df9c6d5e9aca
Mark Longair <mhl@pobox.com>
2017-02-17 16:01:04 +0000

    Add a script to incorporate personal history from Pombola
    This is a quick and hacky script to take person summaries and
    past positions from the Pombola-based ShineYourEye and turn them into
    markdown-with-frontmatter files for prose.io under the 'summaries'
    directory. These are incorporated into the person page for display.
    
    This skips party memberships, since they're likely to be less accurate
    than that from EveryPolitician or any memership where the organization
    or role are missing.

---
+++ bin/build_summaries.rb
@@ -0,0 +1,89 @@
+#!/usr/bin/env ruby
+
+require 'csv'
+require 'date'
+require 'everypolitician'
+require 'open-uri'
+require 'uri'
+
+unless ENV['MORPH_API_KEY']
+  puts "You must set MORPH_API_KEY in order to download the scraper's output."
+  exit(1)
+end
+
+CSV_URL = "https://morph.io/everypolitician-scrapers/nigeria-shineyoureye-positions/data.csv?key=#{URI.escape(ENV['MORPH_API_KEY'])}&query=select+%2A+from+%27data%27"
+
+people = CSV.new(open(CSV_URL), :headers => :first_row).group_by do |row|
+  raise "A person_slug was missing in the data" unless row['person_slug']
+  row['person_slug']
+end
+
+def build_slug_to_uuid
+  map = {}
+  ep_index = EveryPolitician::Index.new()
+  ['Representatives', 'Senate'].each do |house_name|
+    house = ep_index.country('Nigeria').legislature(house_name)
+    house.latest_term.memberships.map(&:person).uniq(&:id).each do |person|
+      identifier_sye = person.identifiers.find { |i| i[:scheme] == 'shineyoureye' }
+      if identifier_sye
+        map[identifier_sye[:identifier]] = person.id
+      end
+    end
+  end
+  map
+end
+
+def format_date(partial_iso8601)
+  return partial_iso8601 if partial_iso8601 =~ /^\d{4}$/
+  if /^\d{4}-\d{2}/ =~ partial_iso8601
+    d = Date.parse(partial_iso8601 + '-01')
+    return d.strftime('%B %Y')
+  end
+  d = Date.parse(partial_iso8601)
+  return d.strftime('%-d %B %Y')
+end
+
+def get_markdown_from_rows(positions)
+  return nil if positions.empty?
+  result = positions[0]['person_summary'].strip
+  result += "\n\n" unless result.empty?
+  for position in positions
+    start_date = position['start_date']
+    end_date = position['end_date']
+    role = position['role']
+    classification = position['organization_classification']
+    organization = position['organization_name']
+    category = position['organization_category']
+    next if classification.include? 'Party'
+    next if organization.empty?
+    next if role.empty?
+    result += "* #{role} at #{organization}"
+    if start_date.empty? and not end_date.empty?
+      result += " until #{format_date(end_date)}"
+    elsif end_date.empty? and not start_date.empty?
+      result += " starting at #{format_date(start_date)}"
+    elsif (not start_date.empty?) and (not end_date.empty?)
+      result += " from #{format_date(start_date)} to #{format_date(end_date)}"
+    end
+    result += "\n"
+  end
+  result += "\n"
+end
+
+slug_to_uuid = build_slug_to_uuid
+
+people.each do |slug, positions|
+  uuid = slug_to_uuid[slug]
+  next unless uuid
+  filename = File.join(
+    File.dirname(__FILE__), '..', 'prose', 'summaries', "#{uuid}.md"
+  )
+  markdown = get_markdown_from_rows(positions)
+  open(filename, 'w') do |f|
+    f.write("---\n")
+    f.write("featured: false\n")
+    f.write("published: true\n")
+    f.write("---\n")
+    f.write(get_markdown_from_rows(positions))
+  end
+end
