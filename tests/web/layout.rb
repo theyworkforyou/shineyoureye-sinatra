90a6d47bf00835f47edb4afe59115987b7033c3f
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-02-24 23:17:13 +0000

    Test untested routes in app.rb: fonts, javascript and robots

--- tests/web/layout.rb
+++ tests/web/layout.rb
@@ -20,4 +20,28 @@ describe 'Layout' do
   it 'includes the footer' do
     refute_empty(subject.css('.site-footer'))
   end
+
+  describe 'fonts' do
+    before  { get '/fonts/bootstrap/glyphicons-halflings-regular.woff2' }
+
+    it 'loads fonts' do
+      last_response.content_type.must_equal('application/font-woff2')
+    end
+  end
+
+  describe 'javascript' do
+    before  { get '/javascripts/bootstrap/bootstrap.min.js' }
+
+    it 'loads javascript' do
+      last_response.content_type.must_equal('application/javascript;charset=utf-8')
+    end
+  end
+
+  describe 'robots' do
+    before  { get '/robots.txt' }
+
+    it 'disallows robots' do
+      last_response.body.must_equal("User-agent: *\nDisallow:\n")
+    end
+  end
 end
