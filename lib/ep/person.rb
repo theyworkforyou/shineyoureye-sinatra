eb6e3b06c7f68900a951827dfd19de44f3fd9d4d
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-02-24 22:06:48 +0000

    Define the allowed images sizes in a class constant
    In this commit we declare more explicity the idea of "allowed
    image sizes" by defining them as a constant that can be then
    used inside the class.
    
    This commit also adds tests for the rest of the logic used to build
    the proxy link and for the exception raised if an image size doesn't
    exist, which for now we are testing using "send", since the method
    is private and we still don't have a reason to expose it publicly as
    part of the person's API.

--- lib/ep/person.rb
+++ lib/ep/person.rb
@@ -13,15 +13,15 @@ module EP
                              :email, :twitter, :facebook, :memberships
 
     def thumbnail_image_url
-      proxy_image_variant('100x100')
+      proxy_image_variant(:thumbnail)
     end
 
     def medium_image_url
-      proxy_image_variant('250x250')
+      proxy_image_variant(:medium)
     end
 
     def original_image_url
-      proxy_image_variant('original')
+      proxy_image_variant(:original)
     end
 
     def twitter_display
@@ -77,6 +77,12 @@ module EP
 
     attr_reader :person, :term, :mapit, :baseurl
 
+    ALLOWED_IMAGE_SIZES = {
+      thumbnail: '100x100',
+      medium: '250x250',
+      original: 'original'
+    }
+
     def ep_area
       current_memberships.first.area
     end
@@ -88,12 +94,12 @@ module EP
     def proxy_image_variant(size)
       raise_unless_image_size_available(size)
       'https://theyworkforyou.github.io/shineyoureye-images' \
-      "/#{legislature.slug}/#{id}/#{size}.jpeg"
+      "/#{legislature.slug}/#{id}/#{ALLOWED_IMAGE_SIZES[size]}.jpeg"
     end
 
     def raise_unless_image_size_available(size)
-      unless ['original', '100x100', '250x250'].include?(size)
-        raise RuntimeException, "Size #{size} is not known to be available"
+      unless ALLOWED_IMAGE_SIZES.has_key?(size)
+        raise "Size #{size} is not known to be available"
       end
     end
   end
