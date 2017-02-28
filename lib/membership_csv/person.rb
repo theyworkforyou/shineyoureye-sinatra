f3eacb08406a2daf3db552ebd5d9fc673bd6c74f
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-02-28 20:35:55 +0000

    Add a person class to deal with person data coming from Morph
    Some fields like twitter or facebook come with several values
    separated by semicolons. To keep the same behavior as the EP
    person class, I am taking the first of those values.
    
    For the same reason, when the phone comes with several values,
    all of them are returned separated by commas as a string.
    
    This class adds a slug method, to be used in the route when we
    want to find a specific governor.
    
    Also. a wikipedia_url method was added although there is no
    wikipedia url coming from morph, because this method is part
    of a person API.

---
+++ lib/membership_csv/person.rb
@@ -0,0 +1,129 @@
+# frozen_string_literal: true
+module MembershipCSV
+  class Person
+    def initialize(person:, mapit:, baseurl:)
+      @person = person
+      @mapit = mapit
+      @baseurl = baseurl
+    end
+
+    def id
+      person['id']
+    end
+
+    def name
+      person['name']
+    end
+
+    def image
+      person['image_url']
+    end
+
+    def birth_date
+      person['birth_date']
+    end
+
+    def phone
+      person['phone'].split(';').join(', ') if person['phone']
+    end
+
+    def email
+      first(person['email']) if person['email']
+    end
+
+    def twitter
+      first(person['twitter']) if person['twitter']
+    end
+
+    def facebook
+      first(person['facebook_url']) if person['facebook_url']
+    end
+
+    def thumbnail_image_url
+      proxy_image_variant(:thumbnail)
+    end
+
+    def medium_image_url
+      proxy_image_variant(:medium)
+    end
+
+    def original_image_url
+      proxy_image_variant(:original)
+    end
+
+    def twitter_display
+      "@#{twitter}" if twitter
+    end
+
+    def twitter_url
+      "https://twitter.com/#{twitter}" if twitter
+    end
+
+    def facebook_display
+      facebook.split('/').last if facebook
+    end
+
+    def facebook_url
+      facebook
+    end
+
+    def wikipedia_url
+      nil
+    end
+
+    def email_url
+      "mailto:#{email}" if email
+    end
+
+    def area
+      mapit.area_from_mapit_name(person['state'])
+    end
+
+    def party_id
+      person['party']
+    end
+
+    def party_name
+      person['party']
+    end
+
+    def party_url
+      "/organisation/#{person['party'].downcase}/"
+    end
+
+    def slug
+      person['identifier__shineyoureye']
+    end
+
+    def url
+      baseurl + slug + '/' if slug
+    end
+
+    private
+
+    attr_reader :person, :mapit, :baseurl
+
+    ALLOWED_IMAGE_SIZES = {
+      thumbnail: '100x100',
+      medium: '250x250',
+      original: 'original'
+    }
+
+    def proxy_image_variant(size)
+      return if image.nil?
+      raise_unless_image_size_available(size)
+      'https://raw.githubusercontent.com/theyworkforyou/shineyoureye-images' \
+      "/gh-pages/Governors/#{id}/#{ALLOWED_IMAGE_SIZES[size]}.jpeg"
+    end
+
+    def raise_unless_image_size_available(size)
+      unless ALLOWED_IMAGE_SIZES.has_key?(size)
+        raise "Size #{size} is not known to be available"
+      end
+    end
+
+    def first(values)
+      values.split(';').first
+    end
+  end
+end
---
+++ tests/membership_csv/person.rb
@@ -0,0 +1,229 @@
+# frozen_string_literal: true
+require 'test_helper'
+require_relative '../../lib/membership_csv/person'
+
+describe 'MembershipCSV::Person' do
+  let(:person) { morph_person(person_filled) }
+
+  describe 'person proxy images' do
+    it 'has a thumbnail image' do
+      person.thumbnail_image_url.must_include('100x100.jpeg')
+    end
+
+    it 'has a URL for a medium-sized image' do
+      person.medium_image_url.must_include('250x250.jpeg')
+    end
+
+    it 'has a URL for the original-sized image' do
+      person.original_image_url.must_include('original.jpeg')
+    end
+
+    it 'uses the legislature slug in the proxy image url' do
+      person.thumbnail_image_url.must_include('Governors')
+    end
+
+    it 'uses the person id in the proxy image url' do
+      person.thumbnail_image_url.must_include('gov:victor-okezie-ikpeazu')
+    end
+
+    it 'throws an exception if the image size does not exist' do
+      error = assert_raises(RuntimeError) { person.send(:proxy_image_variant, :tiny) }
+      error.message.must_include('tiny')
+    end
+
+    it 'returns nil if there is no image' do
+      person = morph_person(person_empty)
+      assert_nil(person.thumbnail_image_url)
+      assert_nil(person.medium_image_url)
+      assert_nil(person.original_image_url)
+    end
+  end
+
+  describe 'person social' do
+    it 'has a Twitter display' do
+      person.twitter_display.must_equal('@GovDickson')
+    end
+
+    it 'has a Twitter url' do
+      person.twitter_url.must_equal('https://twitter.com/GovDickson')
+    end
+
+    it 'returns nil for display if no Twitter' do
+      person = morph_person(person_empty)
+      assert_nil(person.twitter_display)
+    end
+
+    it 'returns nil for url if no Twitter' do
+      person = morph_person(person_empty)
+      assert_nil(person.twitter_url)
+    end
+
+    it 'has a Facebook display' do
+      person.facebook_display.must_equal('OyoStateGovernment')
+    end
+
+    it 'has a Facebook url' do
+      person.facebook_url.must_equal('https://www.facebook.com/OyoStateGovernment')
+    end
+
+    it 'returns nil for the display if no Facebook' do
+      person = morph_person(person_empty)
+      assert_nil(person.facebook_display)
+    end
+
+    it 'returns nil for the url if no Facebook' do
+      person = morph_person(person_empty)
+      assert_nil(person.facebook_url)
+    end
+
+    it 'has an email url' do
+      person.email_url.must_equal('mailto:info@abiastate.gov.ng')
+    end
+
+    it 'returns nil for email url if no email' do
+      person = morph_person(person_empty)
+      assert_nil(person.email_url)
+    end
+  end
+
+  it 'has an id' do
+    person.id.must_equal('gov:victor-okezie-ikpeazu')
+  end
+
+  it 'has a name' do
+    person.name.must_equal('Victor Okezie Ikpeazu')
+  end
+
+  describe 'image' do
+    it 'has an image' do
+      person.image.must_equal('http://www.shineyoureye.org/media_root/images/Abia_-_Okezie_Ikpeazu.jpg')
+    end
+
+    it 'returns nil if no image' do
+      person = morph_person(person_empty)
+      assert_nil(person.image)
+    end
+  end
+
+  describe 'date of birth' do
+    it 'has a date of birth' do
+      person.birth_date.must_equal('1964-10-18')
+    end
+
+    it 'returns nil when no date of birth' do
+      person = morph_person(person_empty)
+      assert_nil(person.birth_date)
+    end
+  end
+
+  describe 'phone' do
+    it 'has a phone' do
+      person.phone.must_equal('7030152466, 08063496033, 08035879790')
+    end
+
+    it 'returns nil when no phone' do
+      person = morph_person(person_empty)
+      assert_nil(person.phone)
+    end
+  end
+
+  describe 'Twitter' do
+    it 'has Twitter' do
+      person.twitter.must_equal('GovDickson')
+    end
+
+    it 'returns nil if no Twitter' do
+      person = morph_person(person_empty)
+      assert_nil(person.twitter)
+    end
+  end
+
+  describe 'Facebook' do
+    it 'has Facebook' do
+      person.facebook.must_equal('https://www.facebook.com/OyoStateGovernment')
+    end
+
+    it 'returns nil if no Facebook' do
+      person = morph_person(person_empty)
+      assert_nil(person.facebook)
+    end
+  end
+
+  it 'does not come with a wikipedia url' do
+    assert_nil(person.wikipedia_url)
+  end
+
+  describe 'email' do
+    it 'has an email' do
+      person.email.must_equal('info@abiastate.gov.ng')
+    end
+
+    it 'returns nil if no email' do
+      person = morph_person(person_empty)
+      assert_nil(person.email)
+    end
+  end
+
+  it 'knows its mapit area id' do
+    person.area.id.must_equal(1)
+  end
+
+  it 'knows its party id' do
+    person.party_id.must_equal('PDP')
+  end
+
+  it 'knows its party name' do
+    person.party_name.must_equal('PDP')
+  end
+
+  it 'knows its party url' do
+    person.party_url.must_equal('/organisation/pdp/')
+  end
+
+  it 'has a slug' do
+    person.slug.must_equal('okezie-ikpeazu')
+  end
+
+  describe 'url' do
+    it 'has a url' do
+      person.url.must_equal('/baseurl/okezie-ikpeazu/')
+    end
+
+    it 'returns nil if no site identifier' do
+      person = morph_person(person_empty)
+      assert_nil(person.url)
+    end
+  end
+
+  def morph_person(person_info)
+    MembershipCSV::Person.new(
+      person: person_info,
+      mapit: FakeMapit.new(1),
+      baseurl: '/baseurl/'
+    )
+  end
+
+  def person_filled
+    {
+      'honorific_prefix' => 'Dr',
+      'name' => 'Victor Okezie Ikpeazu',
+      'id' => 'gov:victor-okezie-ikpeazu',
+      'state' => 'Abia',
+      'party' => 'PDP',
+      'email' => 'info@abiastate.gov.ng;person@example.com',
+      'phone' => '7030152466;08063496033;08035879790',
+      'twitter' => 'GovDickson;DicksonSeriake',
+      'facebook_url' => 'https://www.facebook.com/OyoStateGovernment;http://www.facebook.com/AbiolaAjimobi',
+      'birth_date' => '1964-10-18',
+      'gender' => 'Male',
+      'identifier__shineyoureye' => 'okezie-ikpeazu',
+      'image_url' => 'http://www.shineyoureye.org/media_root/images/Abia_-_Okezie_Ikpeazu.jpg',
+      'website_url' => 'http://ekitistate.gov.ng/',
+      'other_names' => 'Okezie Ikpeazu'
+    }
+  end
+
+  def person_empty
+    person_filled.each_with_object({}) { |(key, _), memo| memo[key] = nil }
+  end
+end
