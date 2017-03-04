3d1820d561246c77199bef71a515161578649b2b
Mark Longair <mhl@pobox.com>
2017-03-04 12:14:27 +0000

    Fix RuboCop's complaints about bin/build_summaries.rb

--- bin/build_summaries.rb
+++ bin/build_summaries.rb
@@ -22,17 +22,21 @@ people = CSV.new(open(CSV_URL), headers: :first_row).group_by do |row|
   row['person_slug']
 end
 
+def slug_to_uuid_for_house(house)
+  map = {}
+  house.latest_term.memberships.map(&:person).uniq(&:id).each do |person|
+    identifier_sye = person.identifiers.find { |i| i[:scheme] == 'shineyoureye' }
+    map[identifier_sye[:identifier]] = person.id if identifier_sye
+  end
+  map
+end
+
 def build_slug_to_uuid
   map = {}
-  ep_index = EveryPolitician::Index.new()
+  ep_index = EveryPolitician::Index.new
   %w(Representatives Senate).each do |house_name|
     house = ep_index.country('Nigeria').legislature(house_name)
-    house.latest_term.memberships.map(&:person).uniq(&:id).each do |person|
-      identifier_sye = person.identifiers.find { |i| i[:scheme] == 'shineyoureye' }
-      if identifier_sye
-        map[identifier_sye[:identifier]] = person.id
-      end
-    end
+    map.merge(slug_to_uuid_for_house(house))
   end
   map
 end
@@ -44,32 +48,53 @@ def format_date(partial_iso8601)
     return d.strftime('%B %Y')
   end
   d = Date.parse(partial_iso8601)
-  return d.strftime('%-d %B %Y')
+  d.strftime('%-d %B %Y')
+end
+
+def only_end(start_date, end_date)
+  start_date.empty? && !end_date.empty?
+end
+
+def only_start(start_date, end_date)
+  end_date.empty? && !start_date.empty?
+end
+
+def both_start_and_end(start_date, end_date)
+  !start_date.empty? && !end_date.empty?
+end
+
+def formatted_date_range(start_date, end_date)
+  if only_end(start_date, end_date)
+    " until #{format_date(end_date)}"
+  elsif only_start(start_date, end_date)
+    " starting at #{format_date(start_date)}"
+  elsif both_start_and_end(start_date, end_date)
+    " from #{format_date(start_date)} to #{format_date(end_date)}"
+  else
+    ''
+  end
+end
+
+def get_markdown_for_position(position)
+  start_date = position['start_date']
+  end_date = position['end_date']
+  role = position['role']
+  organization = position['organization_name']
+  return if position['organization_classification'].include? 'Party'
+  return if organization.empty?
+  return if role.empty?
+  bullet_item = "* #{role} at #{organization}"
+  bullet_item += formatted_date_range(start_date, end_date)
+  bullet_item + "\n"
 end
 
 def get_markdown_from_rows(positions)
   return nil if positions.empty?
   result = positions[0]['person_summary'].strip
   result += "\n\n" unless result.empty?
-  for position in positions
-    start_date = position['start_date']
-    end_date = position['end_date']
-    role = position['role']
-    classification = position['organization_classification']
-    organization = position['organization_name']
-    category = position['organization_category']
-    next if classification.include? 'Party'
-    next if organization.empty?
-    next if role.empty?
-    result += "* #{role} at #{organization}"
-    if start_date.empty? and not end_date.empty?
-      result += " until #{format_date(end_date)}"
-    elsif end_date.empty? and not start_date.empty?
-      result += " starting at #{format_date(start_date)}"
-    elsif (not start_date.empty?) and (not end_date.empty?)
-      result += " from #{format_date(start_date)} to #{format_date(end_date)}"
-    end
-    result += "\n"
+  positions.each do |position|
+    position_markdown = get_markdown_for_position(position)
+    result += position_markdown if position_markdown
   end
   result += "\n"
 end
@@ -82,7 +107,6 @@ people.each do |slug, positions|
   filename = File.join(
     File.dirname(__FILE__), '..', 'prose', 'summaries', "#{uuid}.md"
   )
-  markdown = get_markdown_from_rows(positions)
   open(filename, 'w') do |f|
     f.write("---\n")
     f.write("featured: false\n")
