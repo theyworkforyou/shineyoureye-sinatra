2b46a5bfaf5c80e7a0330bdacc2188bc680a84d0
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-03-08 16:41:23 +0000

    Add a page fragment / web component for a featured person

---
+++ lib/featured_person.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+module PageFragment
+  FeaturedPerson = Struct.new(:featured_person, :people_type_display_text, :people_type_url)
+end
---
+++ tests/featured_person.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+require 'test_helper'
+require_relative '../lib/featured_person'
+
+describe 'FeaturedPerson' do
+  let(:page_fragment) do
+    PageFragment::FeaturedPerson.new(FakePerson.new('Name'), 'Senators', '/url/to/list/of/senators/')
+  end
+
+  it 'has a featured person' do
+    page_fragment.featured_person.name.must_equal('Name')
+  end
+
+  it 'has a display text for the person type' do
+    page_fragment.people_type_display_text.must_equal('Senators')
+  end
+
+  it 'has a url to the list of people of that type' do
+    page_fragment.people_type_url.must_equal('/url/to/list/of/senators/')
+  end
+
+  FakePerson = Struct.new(:name)
+end
