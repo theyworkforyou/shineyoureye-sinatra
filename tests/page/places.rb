0ed43eb383d1132b28c7372d12849ca7f5af9200
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-03-23 12:37:15 +0000

    Check that both a start date and a year exist in the places page
    Use safe navigation operator (&.) for checks (available from Ruby 2.3 on)
    instead of the longer:
    
        people_by_legislature.current_term_start_date.year
          if people_by_legislature.current_term_start_date
    
    The parent place pages don't show a current term start date. The
    information related with the legislature is taken from the people.
    Since parent places receive an instance of people that returns nil
    for the legislature related methods (legislature name and current
    term start date) we have to do this check before we ask it for the
    year.

--- tests/page/places.rb
+++ tests/page/places.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 require 'test_helper'
 require_relative '../../lib/ep/people_by_legislature'
+require_relative '../../lib/membership_csv/people'
 require_relative '../../lib/page/places'
 
 describe 'Page::Places' do
@@ -32,5 +33,35 @@ describe 'Page::Places' do
 
   it 'knows the current term year' do
     page.current_term_start_year.must_equal(2015)
+  end
+
+  describe 'when places are parent areas' do
+    let(:contents) do
+      'id,name,identifier__shineyoureye,phone
+id1,name1,name-1,1234
+id2,name2,name-2,5678
+id3,name3,name-3,'
+    end
+    let(:people) do
+      MembershipCSV::People.new(
+        csv_filename: new_tempfile(contents),
+        person_factory: FakePersonFactory.new(FakeMapit.new(1))
+      )
+    end
+    let(:page) do
+      Page::Places.new(
+        title: 'States',
+        places: %w(irrelevant irrelevant),
+        people_by_legislature: people
+      )
+    end
+
+    it 'does not have a legislature name' do
+      assert_nil(page.legislature_name)
+    end
+
+    it 'does not know the current term year' do
+      assert_nil(page.current_term_start_year)
+    end
   end
 end
