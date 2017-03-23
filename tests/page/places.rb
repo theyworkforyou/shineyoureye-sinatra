5584b05a595fedfea0847a59bde1a83bb52f77c8
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-03-23 12:53:47 +0000

    Separate child tests from parent tests wrapping them in a describe block.

--- tests/page/places.rb
+++ tests/page/places.rb
@@ -5,34 +5,36 @@ require_relative '../../lib/membership_csv/people'
 require_relative '../../lib/page/places'
 
 describe 'Page::Places' do
-  let(:people) do
-    EP::PeopleByLegislature.new(
-      legislature: nigeria_at_known_revision.legislature('Representatives'),
-      person_factory: FakePersonFactory.new(FakeMapit.new(1))
-    )
-  end
-  let(:page) do
-    Page::Places.new(
-      title: 'Constituencies',
-      places: %w(irrelevant irrelevant),
-      people_by_legislature: people
-    )
-  end
+  describe 'when places are child areas' do
+    let(:people) do
+      EP::PeopleByLegislature.new(
+        legislature: nigeria_at_known_revision.legislature('Representatives'),
+        person_factory: FakePersonFactory.new(FakeMapit.new(1))
+      )
+    end
+    let(:page) do
+      Page::Places.new(
+        title: 'Constituencies',
+        places: %w(irrelevant irrelevant),
+        people_by_legislature: people
+      )
+    end
 
-  it 'has a title' do
-    page.title.must_equal('Constituencies')
-  end
+    it 'has a title' do
+      page.title.must_equal('Constituencies')
+    end
 
-  it 'has all areas' do
-    page.places.count.must_equal(2)
-  end
+    it 'has all areas' do
+      page.places.count.must_equal(2)
+    end
 
-  it 'knows the legislature name' do
-    page.legislature_name.must_equal('House of Representatives')
-  end
+    it 'knows the legislature name' do
+      page.legislature_name.must_equal('House of Representatives')
+    end
 
-  it 'knows the current term year' do
-    page.current_term_start_year.must_equal(2015)
+    it 'knows the current term year' do
+      page.current_term_start_year.must_equal(2015)
+    end
   end
 
   describe 'when places are parent areas' do
