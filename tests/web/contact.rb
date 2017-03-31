09ec086978d5104ed1379ee53a8a4d67ef56839e
Zarino Zappia <mail@zarino.co.uk>
2017-03-31 12:16:43 +0100

    Less margin above contact form
    We break the `.search-form` component up into `.big-form` and
    `.big-form--short` that includes the extra margin.

--- tests/web/contact.rb
+++ tests/web/contact.rb
@@ -10,10 +10,10 @@ describe 'Contact Page' do
   end
 
   it 'displays the title on the page' do
-    subject.css('.search-form h1').text.must_include('Contact')
+    subject.css('.big-form h1').text.must_include('Contact')
   end
 
   it 'includes the site email in the contact form' do
-    subject.css('.search-form/@action').text.must_include('user@example.com')
+    subject.css('.big-form/@action').text.must_include('user@example.com')
   end
 end
