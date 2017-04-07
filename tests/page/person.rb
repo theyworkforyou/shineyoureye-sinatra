3daa36db05cbac57f9fd2d43b55983805a3c16ab
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-04-07 09:35:03 +0100

    Move the person interface tests to the CSV person test
    This test was misplaced, the person interface tests job is to
    ensure that both types of person have the same interface, so
    it is less confusing for it to live in the CSV person tests file.

--- tests/page/person.rb
+++ tests/page/person.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 require 'test_helper'
 require_relative '../../lib/ep/people_by_legislature'
-require_relative '../../lib/membership_csv/people'
 require_relative '../../lib/page/person'
 require_relative '../shared_examples/person_interface_test'
 
@@ -32,30 +31,6 @@ describe 'Page::Person' do
 
   describe 'when page is an EP person' do
     include PersonInterfaceTest
-    let(:person) { page.person }
-  end
-
-  describe 'when page is a Morph person' do
-    include PersonInterfaceTest
-    let(:contents) do
-      'identifier__shineyoureye
-foo'
-    end
-    let(:people) do
-      MembershipCSV::People.new(
-        csv_filename: new_tempfile(contents),
-        mapit: 'irrelevant',
-        baseurl: 'irrelevant',
-        identifier_scheme: 'shineyoureye'
-      )
-    end
-    let(:page) do
-      Page::Person.new(
-        person: people.find_single('foo'),
-        position: 'irrelevant',
-        summary_doc: 'irrelevant'
-      )
-    end
     let(:person) { page.person }
   end
 end
