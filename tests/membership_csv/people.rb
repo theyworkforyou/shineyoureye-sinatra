e77f1ec5a5a9080211d67832698b22a44496610f
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-04-10 13:58:19 +0100

    Prepare the CSV people tests for the introduction of a factory
    The introduction of a person factory will simplify the class and
    its tests, so this change reorganizes the test file so that the
    the purely formatting changes don't make it hard to parse the
    real changes.
    
    Basically when we move to person factories in the future, some of
    the tests for the people classes won't be needed anymore as they
    will be moved to be part of the factory class tests.
    
    In particular, those tests dealing with checking that an argument
    passed to the entity class was set correctly will make more sense
    in the factory tests.

--- tests/membership_csv/people.rb
+++ tests/membership_csv/people.rb
@@ -4,88 +4,74 @@ require_relative '../../lib/membership_csv/people'
 require_relative '../shared_examples/people_interface_test'
 
 describe 'MembershipCSV::People' do
+  include PeopleInterfaceTest
+
   let(:contents) do
     'id,name,identifier__shineyoureye,phone
 id1,name1,name-1,1234
 id2,name2,name-2,5678
 id3,name3,name-3,'
   end
-
-  describe 'non-mapit methods' do
-    include PeopleInterfaceTest
-
-    let(:people) do
-      MembershipCSV::People.new(
-        csv_filename: new_tempfile(contents),
-        mapit: 'irrelevant',
-        baseurl: '/baseurl/',
-        identifier_scheme: 'shineyoureye'
-      )
-    end
-
-    it 'finds all people' do
-      people.find_all.count.must_equal(3)
-    end
-
-    it 'finds all people as persons' do
-      people.find_all.first.id.must_equal('id1')
-    end
-
-    it 'uses the baseurl in the person url' do
-      people.find_all.first.url.must_equal('/baseurl/name-1/')
-    end
-
-    it 'returns nil for empty fields' do
-      assert_nil(people.find_all.last.phone)
-    end
-
-    it 'finds all people sorted by name' do
-      sorted_alphabetically = people.find_all.first.name < people.find_all.last.name
-      sorted_alphabetically.must_equal(true)
-    end
-
-    it 'finds by slug' do
-      people.find_single('name-2').name.must_equal('name2')
-    end
-
-    it 'does not know the start date of the current term' do
-      assert_nil(people.current_term_start_date)
-    end
-
-    it 'does not know its legislature name' do
-      assert_nil(people.legislature_name)
-    end
-
-    it 'finds a featured person' do
-      featured_summaries = [
-        FakeSummary.new('id1'),
-        FakeSummary.new('foo')
-      ]
-      people.featured_person(featured_summaries).name.must_equal('name1')
-    end
-
-    it 'returns nil if no featured person' do
-      featured_summaries = [FakeSummary.new('foo'), FakeSummary.new('bar')]
-      assert_nil(people.featured_person(featured_summaries))
-    end
+  let(:people) do
+    MembershipCSV::People.new(
+      csv_filename: new_tempfile(contents),
+      mapit: FakeMapit.new(1),
+      baseurl: '/baseurl/',
+      identifier_scheme: 'shineyoureye'
+    )
   end
 
-  describe 'extra mapit functionality' do
-    let(:people) do
-      MembershipCSV::People.new(
-        csv_filename: new_tempfile(contents),
-        mapit: FakeMapit.new(1),
-        baseurl: '/baseurl/',
-        identifier_scheme: 'shineyoureye'
-      )
-    end
+  it 'finds all people' do
+    people.find_all.count.must_equal(3)
+  end
 
-    it 'assigns a mapit area to the person' do
-      people.find_single('name-3').area.id.must_equal(1)
-    end
+  it 'finds all people as persons' do
+    people.find_all.first.id.must_equal('id1')
+  end
 
-    it 'finds all people in a mapit area' do
-      people.find_all_by_mapit_area(1).count.must_equal(3)
-    end
+  it 'uses the baseurl in the person url' do
+    people.find_all.first.url.must_equal('/baseurl/name-1/')
+  end
+
+  it 'returns nil for empty fields' do
+    assert_nil(people.find_all.last.phone)
+  end
+
+  it 'finds all people sorted by name' do
+    sorted_alphabetically = people.find_all.first.name < people.find_all.last.name
+    sorted_alphabetically.must_equal(true)
+  end
+
+  it 'finds by slug' do
+    people.find_single('name-2').name.must_equal('name2')
+  end
+
+  it 'does not know the start date of the current term' do
+    assert_nil(people.current_term_start_date)
+  end
+
+  it 'does not know its legislature name' do
+    assert_nil(people.legislature_name)
+  end
+
+  it 'finds a featured person' do
+    featured_summaries = [
+      FakeSummary.new('id1'),
+      FakeSummary.new('foo')
+    ]
+    people.featured_person(featured_summaries).name.must_equal('name1')
+  end
+
+  it 'returns nil if no featured person' do
+    featured_summaries = [FakeSummary.new('foo'), FakeSummary.new('bar')]
+    assert_nil(people.featured_person(featured_summaries))
+  end
+
+  it 'assigns a mapit area to the person' do
+    people.find_single('name-3').area.id.must_equal(1)
+  end
+
+  it 'finds all people in a mapit area' do
+    people.find_all_by_mapit_area(1).count.must_equal(3)
   end
 end
