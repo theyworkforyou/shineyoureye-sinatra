c024a4850a7202481b2052a4748c98b9d34e3737
Zarino Zappia <mail@zarino.co.uk>
2018-10-12 12:38:04 +0100

    Group people lists by state

--- lib/page/people.rb
+++ lib/page/people.rb
@@ -13,6 +13,14 @@ module Page
       @people ||= people_by_legislature.find_all
     end
 
+    def people_by_state
+      # TODO: Handle people with missing areas better?
+      # @people_by_state = people.group_by {|person| person.area.state.name}.sort.to_h
+      @people_by_state = people.reject { |p| p.area.nil? }
+                               .group_by { |person| person.area.state.name }
+                               .sort.to_h
+    end
+
     private
 
     attr_reader :people_by_legislature
