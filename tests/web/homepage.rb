61bc5efb6ff4f122a4a4abe7667552651ac7e111
Zarino Zappia <mail@zarino.co.uk>
2018-12-05 12:57:02 +0000

    Check for hidden featured people in tests
    Took the opportunity to tidy up the featured person tests too.

--- tests/web/homepage.rb
+++ tests/web/homepage.rb
@@ -7,44 +7,58 @@ describe 'Homepage' do
   subject { Nokogiri::HTML(last_response.body) }
 
   describe 'featured people' do
-    let(:person) { subject.css('.homepage__reps__rep').first }
+    let(:governors) { subject.css('.homepage__reps .col-sm-4')[0] }
+    let(:senators) { subject.css('.homepage__reps .col-sm-4')[1] }
+    let(:representatives) { subject.css('.homepage__reps .col-sm-4')[2] }
 
-    it 'displays the person medium size image' do
-      person.css('img/@src').text
-            .must_include('250x250.jpeg')
+    it 'displays a medium size image for the first person of each type' do
+      governors.css('.homepage__reps__rep').first.css('img/@src').text
+               .must_include('250x250.jpeg')
+      senators.css('.homepage__reps__rep').first.css('img/@src').text
+              .must_include('250x250.jpeg')
+      representatives.css('.homepage__reps__rep').first.css('img/@src').text
+                     .must_include('250x250.jpeg')
+    end
+
+    it 'hides all but the first person of each type' do
+      governors.css('.homepage__reps__rep/@class')[2].text
+               .must_include('hidden')
+      governors.css('.homepage__reps__rep img/@src')[2]
+               .must_be_nil
+      senators.css('.homepage__reps__rep/@class')[4].text
+              .must_include('hidden')
+      senators.css('.homepage__reps__rep img/@src')[4]
+              .must_be_nil
+      representatives.css('.homepage__reps__rep/@class')[0].text
+                     .wont_include('hidden')
+      representatives.css('.homepage__reps__rep img/@src')[0]
+                     .wont_be_nil
     end
 
     it 'displays all types of people' do
-      subject.css('.homepage__reps__rep .btn-default').map(&:text).map(&:strip).uniq.sort
+      subject.css('.homepage__reps .btn-default').map(&:text).map(&:strip).uniq.sort
              .must_equal(%w[Governors Representatives Senators])
     end
 
-    it 'displays link to Governors' do
-      person.css('.btn-default/@href').text.must_equal('/position/executive-governor/')
+    it 'displays a link to Governors' do
+      governors.css('.btn-default/@href')[0].text
+               .must_equal('/position/executive-governor/')
+      governors.css('.btn-default')[0].text.strip
+               .must_equal('Governors')
     end
 
-    it 'displays the Governors label' do
-      person.css('.btn-default').text.strip.must_equal('Governors')
-    end
-
-    it 'displays link to Senators' do
-      subject.css('.homepage__reps__rep .btn-default/@href')[10].text
-             .must_equal('/position/senator/')
-    end
-
-    it 'displays the Senators label' do
-      subject.css('.homepage__reps__rep .btn-default')[10].text.strip
-             .must_equal('Senators')
+    it 'displays a link to Senators' do
+      senators.css('.btn-default/@href')[0].text
+              .must_equal('/position/senator/')
+      senators.css('.btn-default')[0].text.strip
+              .must_equal('Senators')
     end
 
     it 'displays link to Representatives' do
-      subject.css('.homepage__reps__rep .btn-default/@href')[20].text
-             .must_equal('/position/representative/')
-    end
-
-    it 'displays the Representatives label' do
-      subject.css('.homepage__reps__rep .btn-default')[20].text.strip
-             .must_equal('Representatives')
+      representatives.css('.btn-default/@href')[0].text
+                     .must_equal('/position/representative/')
+      representatives.css('.btn-default')[0].text.strip
+                     .must_equal('Representatives')
     end
   end
 
