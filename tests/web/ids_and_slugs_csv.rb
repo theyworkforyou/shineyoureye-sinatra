79bb87b5f9e27998414c5a6eb2f72c809ce1a4f6
EiEResearch <solomon-omokehinde@CLR-R90SEQC1.rewardsco.co.za>
2019-09-02 18:12:07 +0200

    Fixed test from update

--- tests/web/ids_and_slugs_csv.rb
+++ tests/web/ids_and_slugs_csv.rb
@@ -11,14 +11,14 @@ describe 'The ID <-> slugs CSV file' do
   end
 
   it 'must have a slug for a Representative from EP data' do
-    assert(subject.include?(['765d47ff-a659-4faa-9fc7-d641fee30ac5', 'femi-gbajabiamila']))
+    assert(subject.include?(%w[765d47ff-a659-4faa-9fc7-d641fee30ac5 femi-gbajabiamila]))
   end
 
   it 'must have a slug for a Senator from EP data' do
-    assert(subject.include?(['5744450e-41b7-4d7d-b81a-83f42f34437d', 'tinubu-oluremi']))
+    assert(subject.include?(%w[5744450e-41b7-4d7d-b81a-83f42f34437d tinubu-oluremi]))
   end
 
   it 'must have a slug specified in extra-slugs.csv' do
-    assert(subject.include?(['e5a6c86b-b503-4105-983b-d5c81ab41eaf', 'solomon-olamilekan-adeola']))
+    assert(subject.include?(%w[e5a6c86b-b503-4105-983b-d5c81ab41eaf solomon-olamilekan-adeola]))
   end
 end
